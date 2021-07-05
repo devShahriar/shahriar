a166cc7375bd7d972437b5d571b885ef3972d1f0
Safwan Shaheer <devorein00@gmail.com>
2021-07-05 22:55:09 +0600

    Added Navbar component

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+import classes from "./style.module.css";
+
 export default function Navbar(){
-  return <div className="Navbar">
-    <span>
-      About
-    </span>
-    <span>
-      Skills
-    </span>
-    <span>
-      Experience
-    </span>
+  const [currentSection, setCurrentSection] = useState('about');
+
+  return <div className={`Navbar absolute text-gray-700 flex justify-between w-1/4 transform rotate-90 ${classes.Navbar}`}>
+    {['About', 'Skills', 'Experience'].map(navbarItemLabel => <NavbarItem key={navbarItemLabel} currentSection={currentSection} setCurrentSection={setCurrentSection} label={navbarItemLabel}/>)}
   </div>
+}
+
+function NavbarItem(props){
+  const {label, currentSection, setCurrentSection} = props;
+  return <span className={`cursor-pointer text-base ${classes.NavbarItem} ${currentSection === label.toLowerCase() ? 'font-bold' : ''}`} onClick={()=>setCurrentSection(label.toLowerCase())}>
+    {label}
+  </span>
 }

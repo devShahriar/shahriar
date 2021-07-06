e6b8f25d66b0d397346d59d7624a5fc45b033037
Safwan Shaheer <devorein00@gmail.com>
2021-07-06 11:28:21 +0600

    Updated skill item schema

--- components/Skills/index.jsx
+++ components/Skills/index.jsx
@@ -9,14 +9,8 @@ export default function Skills(){
         <div className="font-bold text-2xl text-primary-500 my-5 cursor-pointer">{name}</div>
         <div className="">
           {items.map(item=>{
-            let label = '', icon = '';
-            if(typeof item === "object"){
-              label = item.label;
-              icon = getIcon(item.icon)
-            } else {
-              label = item;
-              icon = getIcon(item)
-            }
+            const label = item.label;
+            const icon = getIcon(item.icon ?? item.label)
             return <div className="text-secondary-100" key={item}>
               <div className="w-1/4 flex mb-5">
                 <span className="mr-5">{icon}</span>

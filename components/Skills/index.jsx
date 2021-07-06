1fdda0d6802202afd8f286463a8eaa36c0793ce9
Safwan Shaheer <devorein00@gmail.com>
2021-07-06 11:47:34 +0600

    Added SkillItem component

--- components/Skills/index.jsx
+++ components/Skills/index.jsx
@@ -3,26 +3,27 @@ import { getIcon } from "../../utils";
 import ProgressBar from "../ProgressBar";
 import Section from "../Section";
 
+function SkillItem(props){
+  const {label, progress} = props;
+  const icon = getIcon(props.icon ?? label)
+  return <div className="text-secondary-100" key={label}>
+    <div className="flex mb-5 items-center">
+      <span className="mr-5">{icon}</span>
+      <span className="flex-grow">{label}</span>
+      <ProgressBar progress={progress}/>
+    </div>
+  </div>
+}
+
 export default function Skills(){
   return <Section className="skills">
     {skills.map(({name, items})=>{
       return <div key={name} className="">
         <div className="font-bold text-2xl text-primary-500 my-5 cursor-pointer">{name}</div>
         <div className="mb-10">
-          {items.map(item=>{
-            const label = item.label;
-            const icon = getIcon(item.icon ?? item.label)
-            return <div className="text-secondary-100" key={item.label}>
-              <div className="flex mb-5 items-center">
-                <span className="mr-5">{icon}</span>
-                <span className="flex-grow">{label}</span>
-                <ProgressBar progress={item.progress}/>
-              </div>
-            </div>
-          })}
+          {items.map(item=><SkillItem {...item} key={item.label}/>)}
         </div>
       </div>
     })}
   </Section>
 }
-

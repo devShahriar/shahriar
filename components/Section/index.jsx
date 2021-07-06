d31d1625bbfb0b670e9134aceffcc5a4d399e8ab
Safwan Shaheer <devorein00@gmail.com>
2021-07-06 07:43:47 +0600

    Added initial metadata.json

--- components/Section/index.jsx
+++ components/Section/index.jsx
@@ -1,5 +1,15 @@
+import React from 'react';
+import { observe } from 'react-intersection-observer';
+
 export default function Section(props){
-  return <div className={`Section h-screen ${props.className ?? ''}`}>
+  const ref = React.useRef(null);
+  observe(ref.current, ()=>{
+    console.log(props.className)
+  }, {
+    threshold: 1,
+    triggerOnce: true
+  })
+  return <div ref={ref} className={`Section h-screen ${props.className ?? ''}`}>
     {props.children}
   </div>
 }

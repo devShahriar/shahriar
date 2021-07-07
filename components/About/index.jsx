7c6c89db062d5fd22427081ca8ab29907b121c4b
Safwan Shaheer <devorein00@gmail.com>
2021-07-07 19:09:45 +0600

    Added description in about section

--- components/About/index.jsx
+++ components/About/index.jsx
@@ -9,30 +9,37 @@ import Section from "../Section";
 export default function About(){
   const {data} = useContext(RootContext)
 
-  return <Section name="About" className="flex items-center justify-between px-10 flex-col-reverse md:flex-row">
-    <div>
-      <div className="text-5xl text-secondary-50 my-5">{data.about.title}</div>
+  return <Section name="About" className="">
+    <div className="flex items-center justify-between flex-col-reverse md:flex-row">
       <div>
-        <ReactTyped
-          strings={data.about.subtitles}
-          typeSpeed={50}
-          backSpeed={100}
-          className={`font-bold text-primary-500 text-3xl`}
+        <div className="text-5xl text-secondary-50 my-5">{data.about.title}</div>
+        <div>
+          <ReactTyped
+            strings={data.about.subtitles}
+            typeSpeed={50}
+            backSpeed={100}
+            className={`font-bold text-primary-500 text-3xl`}
+          />
+        </div>
+        <div className="text-secondary-300 text-lg my-3">{data.about.bio}</div>
+        <button type="button" className="animate-wiggle hover:animate-none bg-primary-500 px-8 py-3 text-lg rounded font-extrabold my-5 duration-300 ease-in-out flex items-center">
+          <HiDocumentText size={20} fill={colors.gray[900]} className="mr-2"/>
+          <a target="_blank" rel="noreferrer" href={data.about.resume}>View resume</a>
+        </button>
+      </div>
+      <div className="animate-fade-r-bounce">
+        <Image
+          src="/landing.svg"
+          alt="Landing"
+          width="500"
+          height="500"
         />
       </div>
-      <div className="text-secondary-300 text-lg my-3">{data.about.bio}</div>
-      <button type="button" className="animate-wiggle hover:animate-none bg-primary-500 px-8 py-3 text-lg rounded font-extrabold my-5 duration-300 ease-in-out flex items-center">
-        <HiDocumentText size={20} fill={colors.gray[900]} className="mr-2"/>
-        <a target="_blank" rel="noreferrer" href={data.about.resume}>View resume</a>
-      </button>
     </div>
-    <div className="animate-fade-r-bounce">
-      <Image
-        src="/landing.svg"
-        alt="Landing"
-        width="500"
-        height="500"
-      />
+    <div className="text-lg text-secondary-200 font-medium flex justify-end">
+      <div className="w-1/2">
+        {data.about.description}
+      </div>
     </div>
   </Section>
 }

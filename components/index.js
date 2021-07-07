53a33ffb124a79a633a7245b8334516101a6ec4a
Safwan Shaheer <devorein00@gmail.com>
2021-07-07 12:52:21 +0600

    Removed unnecessary components

--- components/index.js
+++ components/index.js
@@ -2,16 +2,12 @@ import About from "./About"
 import Experiences from "./Experiences"
 import Links from "./Links"
 import Navbar from "./Navbar"
-import ProgressBar from "./ProgressBar"
 import Section from "./Section"
 import Skills from "./Skills"
-import Task from "./Task"
 
 export {
   About,
   Experiences,
-  Task,
-  ProgressBar,
   Skills,
   Links,
   Section,

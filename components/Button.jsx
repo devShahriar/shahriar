c5cc113f8877afc3b2b2a7f440cca177616a5b73
Safwan Shaheer <devorein00@gmail.com>
2021-07-09 18:38:27 +0600

    Added go back button on courses page

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,11 +2,11 @@ export default function Button(props){
   const { children, className= '' ,size = 'large', scaleOnHover = false } = props;
   let sizeClasses = '';
   if(size === 'large'){
-    sizeClasses = 'px-8 py-3 text-lg font-extrabold my-5'
+    sizeClasses = 'px-8 py-3 text-lg font-extrabold my-5 mx-5'
   } else if(size === 'medium'){
-    sizeClasses = 'px-6 py-2 text-base font-bold my-3'
+    sizeClasses = 'px-6 py-2 text-base font-bold my-3 mx-3'
   } else if(size === 'small'){
-    sizeClasses = 'px-4 py-2 text-sm font-medium my-2'
+    sizeClasses = 'px-4 py-2 text-sm font-medium my-2 mx-2'
   }
 
   let scaleClasses = '';
--- pages/courses/[slug].jsx
+++ pages/courses/[slug].jsx
@@ -1,6 +1,8 @@
+import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useContext } from 'react';
-import { Typography, Wrapper } from "../../components";
+import { FiArrowLeftCircle } from "react-icons/fi";
+import { Button, Typography, Wrapper } from "../../components";
 import { RootContext } from '../../contexts';
 
 const Course = () => {
@@ -11,7 +13,26 @@ const Course = () => {
   
   let content = null;
   if(course){
-    content = <Typography.H1>{course.title}</Typography.H1>
+    content = <div>
+      <Button scaleOnHover className="absolute top-0 left-0" size={"medium"}><FiArrowLeftCircle size={20} className="mr-3"/><Link href={"/"}><span>Go Back</span></Link></Button>
+      <div className="my-10 flex justify-center">
+        <Typography.H1>{course.title}</Typography.H1>
+      </div>
+      <div className="flex justify-center">
+        {course.video_url ? <video style={{
+          height: 500,
+          width: 500
+        }}>
+          <source src={course.video_url} type="video/mp4"/>
+        </video> : <img style={{
+          height: 500,
+          width: 500
+        }} href={course.image_url} alt="Course image"/>}
+      </div>
+      <div className="flex justify-center">
+        <Typography.Body1>{course.description}</Typography.Body1>
+      </div>
+    </div>
   }else{
     content = <Typography.H1>Course not found !!!</Typography.H1>
   }

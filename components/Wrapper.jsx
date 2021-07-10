345e84f27c90bdef8d39596884175c6eea61bf47
Safwan Shaheer <devorein00@gmail.com>
2021-07-10 20:34:34 +0600

    Renamed Links to Socials

--- components/Wrapper.jsx
+++ components/Wrapper.jsx
@@ -1,10 +1,10 @@
-import { Links, Navbar } from "../components";
+import { Navbar, Socials } from "../components";
 
 export default function Wrapper(props){
   const {children, showNavbar = true} = props;
   return <div className="px-24 py-10 font-mono bg-secondary-900 h-screen overflow-auto scrollbar scrollbar-thumb-green-500 scrollbar-track-gray-100">
     {showNavbar && <Navbar />}
-    <Links />
+    <Socials />
     {children}
   </div>
 }
--- components/index.js
+++ components/index.js
@@ -5,28 +5,28 @@ import Courses from "./Courses"
 import Experiences from "./Experiences"
 import Grid from "./Grid"
 import Icon from "./Icon"
-import Links from "./Links"
 import Logos from "./Logos"
 import Navbar from "./Navbar"
 import Section from "./Section"
 import Skills from "./Skills"
+import Socials from "./Socials"
 import Typography from "./Typography"
 import Wrapper from "./Wrapper"
 
 export {
-  Typography,
-  Wrapper,
-  Card,
-  Logos,
-  Button,
-  Grid,
-  Icon,
-  Courses,
-  About,
-  Experiences,
-  Skills,
-  Links,
-  Section,
-  Navbar
+    Typography,
+    Wrapper,
+    Card,
+    Logos,
+    Button,
+    Grid,
+    Icon,
+    Courses,
+    About,
+    Experiences,
+    Skills,
+    Socials,
+    Section,
+    Navbar
 }
 
